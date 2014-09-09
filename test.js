09623ef59d4396a810cbc1ed36354307e1f32323
pierreolivier <bouteau.pierreolivier@gmail.com>
2014-09-09 22:37:26 +0200

    all cmds

--- test.js
+++ test.js
@@ -3,14 +3,22 @@ var urlshortener = require('./index');
 urlshortener.connect('127.0.0.1:3001', 'po', function (connected, err) {
     if (connected) {
         var args = {protection: 'password', password: 'test', long_key: 1, timeout: 300};
-        urlshortener.shorten('http://google.com', args, function(shortUrl, err) {
+        urlshortener.shorten('http://google.com', args, function(shortUrl, err2) {
             console.log(shortUrl);
+
+            urlshortener.expand(shortUrl, function(urls, err3) {
+                console.log(urls);
+            });
         });
 
-        urlshortener.shorten('http://google.fr', function(shortUrl, err) {
+        urlshortener.shorten('http://google.fr', function(shortUrl, err2) {
             console.log(shortUrl);
+
+            urlshortener.delete(shortUrl);
         });
 
-        urlshortener.shorten('http://www.google.com');
+        urlshortener.list(function(urls, err2) {
+            console.log(urls);
+        });
     }
 });

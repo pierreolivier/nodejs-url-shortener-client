f796de8fee0901c4576614f43cab6c16779ff53d
pierreolivier <bouteau.pierreolivier@gmail.com>
2014-09-09 17:52:57 +0200

    connect + shorten

--- test.js
+++ test.js
@@ -1,1 +1,16 @@
 var urlshortener = require('./index');
+
+urlshortener.connect('127.0.0.1:3001', 'po', function (connected, err) {
+    if (connected) {
+        var args = {protection: 'password', password: 'test', long_key: 1, timeout: 300};
+        urlshortener.shorten('http://google.com', args, function(shortUrl, err) {
+            console.log(shortUrl);
+        });
+
+        urlshortener.shorten('http://google.fr', function(shortUrl, err) {
+            console.log(shortUrl);
+        });
+
+        urlshortener.shorten('http://www.google.com');
+    }
+});
